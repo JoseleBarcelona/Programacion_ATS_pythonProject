ffd493da3a4e076f1620541d19bd396ce9eb282a
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-03-11 14:04:38 +0100

    Add files via upload

---
+++ Operadores/OperadoresAritmeticos.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 3
+resultado = num1 + num2
+resultado1 = num1 * num2
+resultado2 = num1 / num2  # Con / el resultado es exacto
+resultado3 = num1 // num2  # Con // el resultado es entero, redondea a la baja
+resultado4 = num1 % num2  # Esto te da el resto de una división
+
+print(resultado, resultado1, resultado2, resultado3, resultado4)
+print(resultado + resultado1)
+
+num3 = 5
+num4 = 2
+potencia = num3 ** num4  #Esto ** te hace la operación de la potencia, base, exponente
+print(potencia)
+
+resultado5 = 3**3 * (13/5 - (2*4))#las operaciones aritméticas van desde los paréntesis interiores, hacia afuera.
+print(resultado5)
---
+++ Operadores/OperadoresAsignacion.py
@@ -0,0 +1,10 @@
+a = 0
+a = a + 5 # una manera de onstanciarlo
+a += 5 # otra manera de instanciarlo
+a -=2
+a *=2
+a /=4
+a **= 2
+a %= 2
+
+print(a)
